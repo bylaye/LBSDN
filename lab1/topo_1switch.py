1400b555c55a870232044a7128438dc23f90c5ac
layebinaire <layebinaire@gmail.com>
2022-05-28 23:07:59 +0000

    topology with single switch and 8 hosts

---
+++ lab1/topo_1switch.py
@@ -0,0 +1,31 @@
+from mininet.topo import Topo
+
+class MyTopo( Topo ):
+    "Simple topology example."
+
+    def build( self ):
+        "Create custom topo."
+
+        # Add hosts and switches
+        h1 = self.addHost( 'h1' )
+        h2 = self.addHost( 'h2' )
+        h3 = self.addHost( 'h3' )
+        h4 = self.addHost( 'h4' )
+        h5 = self.addHost( 'h5' )
+        h6 = self.addHost( 'h6' )
+        h7 = self.addHost( 'h7' )
+        h8 = self.addHost( 'h8' )
+        s1 = self.addSwitch( 's1' )
+
+        # Add links
+        self.addLink( h1, s1 )
+        self.addLink( h2, s1 )
+        self.addLink( h3, s1 )
+
+        self.addLink( h4, s1 )
+        self.addLink( h5, s1 )
+        self.addLink( h6, s1 )
+        self.addLink( h7, s1 )
+        self.addLink( h8, s1 )
+
+topos = { 'mytopo': ( lambda: MyTopo() ) }
